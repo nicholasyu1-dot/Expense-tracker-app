69d5c3b9ab9db3012083939f4ef3970f77b77a8b
Nicholas <you@example.com>
2026-03-05 15:31:47 -0500

    moved to 2 files yeah we have more files

---
+++ Code/Button_menus.py
@@ -0,0 +1,100 @@
+
+from tkinter import *
+from tkinter import ttk, messagebox
+
+
+class Menu:
+
+    def show_add_expenses_window(self):
+        self.amount.grid(row=0, column=0, padx=20, pady=20, ipadx=10, ipady=10)
+        self.date.grid(column=0, row=1, padx=20, pady=20)
+        self.type_of_expense.grid(column=0, row=2, padx=20, pady=20)
+        self.note_label.grid(column=0, row=3, padx=20, pady=20)
+
+        self.amount_entry.grid(column=1, row=0, padx=20, pady=20)
+        self.date_entry.grid(column=1, row=1, padx=20, pady=20)
+        self.type_dropdown.grid(column=1, row=2, padx=20, pady=20)
+        self.note_entry.grid(column=1, row=3, padx=20, pady=20)
+
+        self.save_button.grid(column=0, row=4, columnspan=2, pady=20)
+
+
+    def setup_add_expenses_window(self,expenses,screen):
+        self.add_expenses_window = Toplevel(screen)
+        self.add_expenses_window.title("Add an expense")
+        self.add_expenses_window.geometry(
+            f"1000x700+{screen.winfo_screenwidth() // 2 - 1000 // 2}+{screen.winfo_screenheight() // 2 - 700 // 2}"
+        )
+        self.add_expenses_window.focus_force()
+        self.add_expenses_window.grab_set()
+        self.add_expenses_window.configure(bg="#34495e")
+
+        self.amount_value = StringVar()
+        self.date_value = StringVar()
+        self.note_value = StringVar()
+        self.selected_type = StringVar(value="select an option")
+
+        self.amount = ttk.Label(self.add_expenses_window, text="Amount spent")
+        self.date = ttk.Label(self.add_expenses_window, text="Select a date")
+        self.type_of_expense = ttk.Label(self.add_expenses_window, text="Select a type")
+        self.note_label = ttk.Label(self.add_expenses_window, text="Note")
+
+        self.amount_entry = ttk.Entry(self.add_expenses_window, textvariable=self.amount_value)
+        self.date_entry = ttk.Entry(self.add_expenses_window, textvariable=self.date_value)
+        self.note_entry = ttk.Entry(self.add_expenses_window, textvariable=self.note_value)
+
+        self.options = ["select an option", "Food", "Clothing", "Medicine", "video games", "Custom"]
+        self.type_dropdown = ttk.OptionMenu(
+            self.add_expenses_window, self.selected_type, *self.options
+        )
+
+        self.save_button = ttk.Button(
+            self.add_expenses_window,
+            text="Save Expense",
+            command=lambda: self.save_expense(expenses)
+        )
+
+
+    def create_add_expenses_window(self,expenses,screen):
+        print(expenses)
+
+        self.setup_add_expenses_window(expenses,screen)
+        self.show_add_expenses_window()
+
+
+
+    def save_expense(self,expenses):
+        try:
+            amount = int(self.amount_value.get())
+            category = self.selected_type.get()
+            date_string = self.date_value.get()
+            note = self.note_value.get()
+
+            expense = {
+                "amount": amount,
+                "category": category,
+                "date": date_string,
+                "note": note
+            }
+
+            expenses.append(expense)
+
+            messagebox.showinfo("Success", "Expense saved successfully")
+            self.add_expenses_window.destroy()
+
+
+        except ValueError as e:
+            messagebox.showerror("Error", str(e))
+
+
+
+
+    def setup_view_expenses_window(self,expenses,screen):
+        self.view_expenses_window = Toplevel(screen)
+        self.expenses = expenses
+
+
+
+    def create_view_expenses_window(self,expenses,screen):
+        self.setup_view_expenses_window(expenses,screen)
+        #self.show_view_expenses_window()
--- Code/Expense-tracker-app.py
+++ Code/Expense-tracker-app.py
@@ -1,7 +1,6 @@
-from tkinter import *
-from tkinter import ttk, messagebox
 
-
+from Button_menus import *
+from sqlite3 import *
 class App(Tk):
     def __init__(self):
         super().__init__()
@@ -16,85 +15,8 @@ class App(Tk):
         self.create_layout()
 
         self.bind("<Escape>", self.toggle_fullscreen)
+        self.Button_menus = Menu()
 
-
-    def show_add_expenses_window(self):
-        self.amount.grid(row=0, column=0, padx=20, pady=20, ipadx=10, ipady=10)
-        self.date.grid(column=0, row=1, padx=20, pady=20)
-        self.type_of_expense.grid(column=0, row=2, padx=20, pady=20)
-        self.note_label.grid(column=0, row=3, padx=20, pady=20)
-
-        self.amount_entry.grid(column=1, row=0, padx=20, pady=20)
-        self.date_entry.grid(column=1, row=1, padx=20, pady=20)
-        self.type_dropdown.grid(column=1, row=2, padx=20, pady=20)
-        self.note_entry.grid(column=1, row=3, padx=20, pady=20)
-
-        self.save_button.grid(column=0, row=4, columnspan=2, pady=20)
-
-
-    def setup_add_expenses_window(self):
-        self.add_expenses_window = Toplevel(self)
-        self.add_expenses_window.title("Add an expense")
-        self.add_expenses_window.geometry(
-            f"1000x700+{self.winfo_screenwidth() // 2 - 1000 // 2}+{self.winfo_screenheight() // 2 - 700 // 2}"
-        )
-        self.add_expenses_window.focus_force()
-        self.add_expenses_window.grab_set()
-        self.add_expenses_window.configure(bg="#34495e")
-
-        self.amount_value = StringVar()
-        self.date_value = StringVar()
-        self.note_value = StringVar()
-        self.selected_type = StringVar(value="select an option")
-
-        self.amount = ttk.Label(self.add_expenses_window, text="Amount spent")
-        self.date = ttk.Label(self.add_expenses_window, text="Select a date")
-        self.type_of_expense = ttk.Label(self.add_expenses_window, text="Select a type")
-        self.note_label = ttk.Label(self.add_expenses_window, text="Note")
-
-        self.amount_entry = ttk.Entry(self.add_expenses_window, textvariable=self.amount_value)
-        self.date_entry = ttk.Entry(self.add_expenses_window, textvariable=self.date_value)
-        self.note_entry = ttk.Entry(self.add_expenses_window, textvariable=self.note_value)
-
-        self.options = ["select an option", "Food", "Clothing", "Medicine", "video games", "Custom"]
-        self.type_dropdown = ttk.OptionMenu(
-            self.add_expenses_window, self.selected_type, *self.options
-        )
-
-        self.save_button = ttk.Button(
-            self.add_expenses_window,
-            text="Save Expense",
-            command=self.save_expense
-        )
-
-
-    def create_add_expenses_window(self):
-        self.setup_add_expenses_window()
-        self.show_add_expenses_window()
-
-
-    def save_expense(self):
-        try:
-            amount = int(self.amount_value.get())
-            category = self.selected_type.get()
-            date_string = self.date_value.get()
-            note = self.note_value.get()
-
-
-            expense = {
-                "amount": amount,
-                "category": category,
-                "date": date_string,
-                "note": note
-            }
-
-            self.expenses.append(expense)
-
-            messagebox.showinfo("Success", "Expense saved successfully")
-            self.add_expenses_window.destroy()
-
-        except ValueError as e:
-            messagebox.showerror("Error", str(e))
 
     def toggle_fullscreen(self, event=None):
         self.is_fullscreen = not self.is_fullscreen
@@ -157,7 +79,7 @@ class App(Tk):
         self.add_expense_btn = ttk.Button(
             self.left_frame,
             text="+ Add Expense",
-            command=self.create_add_expenses_window
+            command=lambda: self.Button_menus.create_add_expenses_window(self.expenses,self)
         )
         self.add_expense_btn.grid(row=0, column=0, sticky="nw", pady=(30, 10), padx=30)
 
